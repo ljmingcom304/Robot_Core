9cc7d8433a2f81afb59910693c104282e75105c0
ljming <jmliang@mmednet.com>
2019-07-18 13:40:23 +0800

    DEV 增加文本变化监听

---
+++ library/src/main/java/com/mmednet/library/listener/SimpleTextWatcher.java
@@ -0,0 +1,29 @@
+package com.mmednet.library.listener;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+/**
+ * Title:SimpleTextWatcher
+ * <p>
+ * Description:文本变化监听
+ * </p>
+ * Author Jming.L
+ * Date 2019/7/18 13:39
+ */
+public class SimpleTextWatcher implements TextWatcher {
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+}
