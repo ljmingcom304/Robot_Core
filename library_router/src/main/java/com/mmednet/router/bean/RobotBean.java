18fd0a1323cc33ddf8efb04c0a2f20dabec41a2c
shengtingting <wodestt@126.com>
2020-09-23 17:11:54 +0800

    DEV 废弃Router

--- library_router/src/main/java/com/mmednet/router/bean/RobotBean.java
+++
@@ -1,8 +0,0 @@
-package com.mmednet.router.bean;
-
-import com.mmednet.router.BaseBean;
-
-
-public class RobotBean extends BaseBean {
-    private static final long serialVersionUID = 6044948826743091006L;
-}
